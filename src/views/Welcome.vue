4e2f081ad972680f315552e75087f4d9ffba21b5
MinKhant610 <minkhantiopp@gmail.com>
2023-05-15 14:00:35 +0630

    first commit

---
+++ src/views/Welcome.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="welcome container">
+        <p>Welcome</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style>
+    .welcome{
+        padding: 20px 0;
+        text-align: center;
+    }
+</style>
